c99692238202232de1b64993006e62bc3e6474f3
galtoubul <galtoubul@gmail.com>
2022-07-07 10:56:23 +0300

    cleaning favicon related left overs

--- src/ex4/server.js
+++ src/ex4/server.js
@@ -1,17 +1,11 @@
 import express from "express";
-import favicon from "serve-favicon";
 import { logger } from "./server/middlewares/logger.js";
 import { todoRouter } from "./server/routes/todoRouter.js";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
 import cors from "cors";
 
 const port = 8000;
 
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 app.use(express.json());
 app.use(cors());
