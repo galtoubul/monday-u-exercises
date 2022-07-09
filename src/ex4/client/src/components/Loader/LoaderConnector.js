a4801e8ad8a8bd59ea6d45ce7f2d2f5ce0062162
galtoubul <galtoubul@gmail.com>
2022-07-09 22:27:45 +0300

    removed loader connector since it is fully controlled by its parent

--- src/ex4/client/src/components/Loader/LoaderConnector.js
+++
@@ -1,8 +0,0 @@
-import { connect } from "react-redux";
-import Loader from "./Loader";
-
-const mapStateToProps = (state) => {
-  return { isLoading: state.isLoading };
-};
-
-export default connect(mapStateToProps, null)(Loader);
